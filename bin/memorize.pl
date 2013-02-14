04967b503034747fbf5f67b11aa0f019680c7102
Logan Bell <loganbell@gmail.com>
2013-02-13 22:53:27 -0800

    Added command line interface (CLI)
    * Added missing dependencies in dist.ini
    * Created memorize.pl
    * Added MooseX::Getopt usage.

---
+++ bin/memorize.pl
@@ -0,0 +1,6 @@
+#! /usr/bin/env perl
+
+use strictures;
+use Cmd::Memorize;
+
+Cmd::Memorize->new->start;
--- lib/Cmd/Memorize.pm
+++ lib/Cmd/Memorize.pm
@@ -1,5 +1,29 @@
-use strict;
-use warnings;
 package Cmd::Memorize;
+use Moose;
+use namespace::autoclean;
 
-1;
+with 'MooseX::Getopt';
+
+has play  => ( is => 'rw', isa => 'Bool', default => 1 );
+has add   => ( is => 'rw', isa => 'Bool', );
+
+sub start {
+    my $self = shift;
+
+    $self->add ?  $self->_handle_add :
+    $self->play ? $self->_handle_play : $self->_handle_error;
+}
+
+sub _handle_add {
+    # Todo implement
+}
+
+sub _handle_play {
+    # Todo implement
+}
+
+sub _handle_error {
+    # Todo
+}
+
+__PACKAGE__->meta->make_immutable;
