681ec0e91cf536951e89350b6f430013a8c7574e
Logan Bell <loganbell@gmail.com>
2013-02-22 16:17:59 -0800

    making things more complicated

--- lib/Cmd/Memorize/View/Add.pm
+++ lib/Cmd/Memorize/View/Add.pm
@@ -4,31 +4,72 @@ use namespace::autoclean;
 use Curses::UI;
 with 'Cmd::Memorize::View::Wrapper';
 
-has create_set_entry => (
+has screens => (
     is => 'rw',
-    lazy => 1,
-    default => sub {
-        $_[0]->window->add('mytextentry', 'TextEntry', -border => 1,  -text => "C",-x => 14, -y => 2, -label => 'foobar');    
-    }
+    isa => 'HashRef',
+    default => sub { +{ 
+        'show_sets' => 'Show Sets',
+        'create_set' => 'Create Set'
+   } },
 );
 
-has create_set_entry_label => (
-    is => 'rw',
-    lazy => 1,
-    default => sub {
-        $_[0]->window->add('card_label', 'Label', -border => 1, -text => 'Front of Card');
-    }
-);
+sub _build_show_sets {
+    my ($self, $window) = @_;
+}
 
-sub create_set {
-    my $self = shift;
-    my $text = $self->create_set_entry->get();
+sub _build_create_set {
+    my ($self, $window) = @_;
+
+    $window->add(
+        undef, 
+        'Buttonbox',
+        -y => 7,    
+        -buttons => [
+            { 
+                -label => "< Create Set>",
+                -onpress => sub {
+                    my $button = shift;
+                    my $set = $button->root->question(
+                        -question => "What would you like the hint or the front of the card to say?"
+                        -title    => 'Add Set Front',
+                    );
+                    if ($set) {
+                        $button->root->dialog("Ok, adding this set: $set");
+                    }
+                    else {
+                        $button->root->dialog("Ok I guess not");
+                    }
+                } 
+            },
+        ],
+    );
+
+    $window->add(
+        undef, 'Listbox',
+        -y          => 8,
+        -padbottom  => 2,
+        -values     => [],
+        -labels     => {},
+        -width      => 20,
+        -border     => 1,
+        -title      => 'Listbox',
+        -vscrollbar => 1,
+        -onchange   => sub { $self->listbox_callback(@_) },
+    );
 }
 
 sub display {
     my $self = shift;
     my $window = $self->window;
-
 }
 
-__PACKAGE__->meta->make_immutable;
+sub listbox_callback {
+    my $listbox = shift;
+    my $label = $listbox->parent->getobj('listboxlabel');
+    my @sel = $listbox->get;
+    @sel = ('<none>') unless @sel;
+    my $sel = "selected: " . join (", ", @sel);
+    $label->text($listbox->title . " $sel");
+}
+
+1;
--- lib/Cmd/Memorize/View/Wrapper.pm
+++ lib/Cmd/Memorize/View/Wrapper.pm
@@ -3,6 +3,16 @@ package Cmd::Memorize::View::Wrapper;
 use Moose::Role;
 requires 'display';
 
+sub _build_screens {
+    my $self = shift;
+    my $screens = {};
+    while (my ($id, $title) = each %{$self->screens} ) { 
+        $screens->{$id} = $self->cui->add($id, 'Window', -title => $title, %{$self->window_args});
+        $self->${\("_build_${id}")}($screens->{$id});
+    }
+    return $screens;
+}
+
 has cui => (
     is => 'rw',
     required => 1,
@@ -11,9 +21,29 @@ has cui => (
     }
 );
 
+has running_screens => (
+    is => 'rw',
+    isa => 'HashRef',
+    lazy => 1,
+    builder => '_build_screens', 
+);
+
 has window => (
     is => 'rw',
 );
+
+has window_args => (
+    is => 'ro',
+    isa => 'HashRef',
+    default => sub { +{
+        -border       => 1, 
+        -titlereverse => 0, 
+        -padtop       => 2, 
+        -padbottom    => 3, 
+        -ipad         => 1,
+    }},
+);
+
 
 around display => sub {
     my ($next, $self) = (shift, shift);
@@ -21,13 +51,28 @@ around display => sub {
     my $cui = $self->cui;
     my @menu = ({ -label => 'File', -submenu => [
                 { -label => 'Create Set', -value => sub { $self->create_set } },
+                { -label => 'Show Sets', -value => sub { $self->show_sets } },
                 { -label => 'Exit      ^Q', -value => sub { $self->exit_dialog } }
             ] 
         }
     );
+
+
     my $menu = $cui->add( 'menu','Menubar', -menu => \@menu, -fg  => "blue",);
     $self->window($cui->add('window', 'Window',-intellidraw=>1, -y => 1, -bfg => 'red'));
     $self->$next(@_);
+
+    for my $id ( keys %{$self->screens} ) {
+        $self->meta->add_method($id => sub {
+            my $self = shift;
+            $self->running_screens->{$id}->focus;
+        });
+    }
+
+    $cui->set_binding( sub{ exit }, "\cQ" );
+    $cui->set_binding( sub{ shift()->root->focus('menu') }, "\cX" );
+    
+
     $cui->mainloop;
 };
 
